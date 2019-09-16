9e2da6fc8849e5bc9e4afca10f687001eb5fa8cd
ernestoramflovlz <50472477+ernestoramflovlz@users.noreply.github.com>
2019-09-15 20:57:51 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,81 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
+System.Collections.Generic 
 
-
-namespace Bici
+namespace ListaPelicula
 {
-    class BIci
+    class pelicula 
     {
+      private string titulo;
+      private int16 año;
+      private string pais;
+      private string director;
 
-        private string marca;
-        private int current_velocity;
-        private int  velocities;
-        private Persona dueño;
-        
-        public void print()
-        {
-            Console.WriteLine("Marca: (0) Color: (1)", marca, current_velocity);
-        }
-        public void init(string marca, int cv, int vs)
-        {
-            this.marca = marca;
-            velocities = vs;
-        }
-     public void sube();
-     
+      public pelicula (string titulo) {this.titulo=ti;}
+      public void SetTitulo(string t){titulo = t;}
+      public string GetTitulo(){return titulo;}
 
-    }    
+      public pelicula (int16 año) {this.año=an;}
+      public void SetAño(string a){año = a;}
+      public string GetAño(){return año;}
 
-    class Persona
+      public pelicula (string pais) {this.pais=pa;}
+      public void SetTitulo(string p){titulo = p;}
+      public string GetTitulo(){return pais;}
+
+      public pelicula (string director) {this.director=di;}
+      public void SetTitulo(string d){titulo = d;}
+      public string GetTitulo(){return director;}
+      public void imprime()
+
     {
-        private string nombre;
-        private string correos;
-        
-        private List<BIci> bicis;
-
-        public void init (string n, string c)
-        nombre = n;
-        correos = c;
-    
+        Console.WriteLine("Informacion de la pelicula es: {0}", titulo, año, pais, director);
     }
-    public void print()
-    {
-        console.WriteLine("(0) : (1)"), nombre, correos);
+     public pelicula(){titulo = "n/a",año = "n/a", pais = "n/a",director = "n/a";}
     }
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            BIci bici01 = new Bici();
-            bici01.init ("Bike", 2 , 12);
-           // bici01.sube();
-            bici01.print();
+            List<Persona> personas = new List<Persona>();
 
-           BIci bici02 = new Bici();
-            bici01.init ("Pinestar", 0 , 15);
-           // bici01.sube();
-            bici01.print();
+             persona.Add(new Persona ("pam"));
+             persona.Add(new Persona ("tom"));
+             persona.Add(new Persona ("jim"));
 
-            BIci bici03 = new Bici();
-            bici01.init ("Pach", 1 , 10);
-           // bici01.sube();
-            bici01.print();
+             persona.ForEach(print);
 
-            List<BIci> bicis = new List<BICI>();
-            bicis.Add (bici01);
-            bicis.Add (bici02);
-            bicis.Add (bici03);
-            
-
-            foreach(BIci b int bicis){
-                b.print();
-            }
-
-            Persona tom = new Persona();
-            tom.init("Tom, tom@gmail.com");
-            tom.Print();
-
-          //Console.Writeline("Hello World");
+             persona.ForEach(delegate(string persona)
+             {
+                 console.WriteLine(persona);
+             });
+              private static void Print(string p)
+             {
+                Console.WriteLine(p);
+             }
+       
         }
     }
 }
