1143388bd2bd5944d11cec54625cf6565abdade0
ernestoramflovlz <50472477+ernestoramflovlz@users.noreply.github.com>
2019-09-20 21:11:31 -0700

    Update Program.cs

--- Programa básico Películas/pelicula/Program.cs
+++ Programa básico Películas/pelicula/Program.cs
@@ -1,35 +1,35 @@
-﻿using System;
+using System;
 
 namespace Pelicula
 {
-    class pelicula 
+    class Movie 
     {
       private string titulo;
       private int16 año;
       private string pais;
       private string director;
 
-      public pelicula (string titulo) {this.titulo=ti;}
+      public Movie (string titulo) {this.titulo=ti;}
       public void SetTitulo(string t){titulo = t;}
       public string GetTitulo(){return titulo;}
 
-      public pelicula (int16 año) {this.año=an;}
+      public Movie (int16 año) {this.año=an;}
       public void SetAño(string a){año = a;}
       public string GetAño(){return año;}
 
-      public pelicula (string pais) {this.pais=pa;}
-      public void SetTitulo(string p){titulo = p;}
-      public string GetTitulo(){return pais;}
+      public Movie (string pais) {this.pais=pa;}
+      public void SetPais(string p){titulo = p;}
+      public string GetPais(){return pais;}
 
-      public pelicula (string director) {this.director=di;}
-      public void SetTitulo(string d){titulo = d;}
-      public string GetTitulo(){return director;}
+      public Movie (string director) {this.director=di;}
+      public void SetDirector(string d){director = d;}
+      public string GetDirector(){return director;}
       public void imprime()
 
     {
         Console.WriteLine("Informacion de la pelicula es: {0}", titulo, año, pais, director);
     }
-     public pelicula(){titulo = "n/a",año = "n/a", pais = "n/a",director = "n/a";}
+     public Movie(){titulo = "n/a",año = "n/a", pais = "n/a",director = "n/a";}
     }
     class Program
     {
@@ -39,14 +39,19 @@ namespace Pelicula
            Pelicula p1 = new Pelicula();
            p1.SetTitulo("Titanic");
            p1.SetAño(1997); 
-           Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+           p1.SetDirector("James Cameron");
+           p1.SetPais("EUA");
+           Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño(), p1.GetPais(), p1.GetDirector());
            p1.imprime();
 
            Pelicula p2 = new Pelicula();
            p2.SetTitulo("Avatar");
            p2.SetAño(2009); 
-           Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
+           p2.SetDirector("James Cameron");
+           p2.SetPais("EUA");
+           Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño, p2.GetPais(), p2.GetDirector());
            p2.imprime();
         }
     }
 }
+
