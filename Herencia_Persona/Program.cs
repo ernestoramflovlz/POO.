37bddbeec93a87a03601171a79df9ef0c0013f4d
ernestoramflovlz <50472477+ernestoramflovlz@users.noreply.github.com>
2019-10-15 17:51:11 -0700

    Add files via upload

---
+++ Herencia_Persona/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Herencia_Persona
+{
+   class Herencia_Persona
+   {
+   protected string nombre;
+   public string Nombre {get {return nombre;}}
+   public Herencia_Persona(string n) {nombre=n;}
+   public void print(){Console.WriteLine("Mi nombre es:", nombre);}
+   }
+class Alumno: Herencia_Persona
+{
+protected string num_control;
+public Alumno(string nombre, string num_control):base(nombre)
+{
+this.num_control = num_control;
+}
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Herencia_Persona tom = new Herencia_Persona("Tom");
+            Herencia_Persona jim = new Herencia_Persona("Jim");
+            tom.print();
+            jim.print();
+            Alumno ram = new Alumno("ram","18210476");
+            ram.print();
+        }
+    }
+}
